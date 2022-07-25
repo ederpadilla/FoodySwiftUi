c63ad4c2520702000fa2b8d082765883fe221fdc
€%er Padilla! <eder.padilla97@gmial.com>
2022-07-25 13:50:09 -0500

    Initial Commit

---
+++ FoodySwiftUI/FoodySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodySwiftUIApp.swift
+//  FoodySwiftUI
+//
+//  Created by Eder  Padilla on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
