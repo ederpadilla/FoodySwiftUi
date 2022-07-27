d5aa45c0e3bb3d7488080115a520254654d0a122
€%er Padilla! <eder.padilla97@gmial.com>
2022-07-27 16:52:04 -0500

    Refactor detail view

--- FoodySwiftUI/views/uiviews/home/FoodyDetailView.swift
+++ FoodySwiftUI/views/uiviews/home/FoodyDetailView.swift
@@ -14,8 +14,7 @@ struct FoodyDetailView: View {
     
     var body: some View {
         VStack {
-            Image("asian-flank-steak")
-                .resizable()
+            FoodyRemoteImage(url: foody.image)
                 .aspectRatio(contentMode: .fit)
                 .clipped()
                 .frame(maxWidth: .infinity,
@@ -23,18 +22,7 @@ struct FoodyDetailView: View {
             
             Spacer()
             
-            VStack {
-                Text(foody.name)
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                
-                Text(foody.description)
-                    .multilineTextAlignment(.center)
-                    .font(.body)
-                    .padding(8)
-                
-                NutritionFoodyView(foody: foody)
-            }
+            FoodyTitlesView(foody: foody)
             
             Spacer()
             
@@ -59,6 +47,27 @@ struct FoodyDetailView: View {
         .shadow(radius: 40)
         .overlay(XMarkButtonView(isShowingView: $isShowingView),
                  alignment: .topTrailing)
+    }
+}
+
+struct FoodyTitlesView: View {
+    
+    var foody: Foody
+    
+    var body: some View {
+        
+        VStack {
+            Text(foody.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+            
+            Text(foody.description)
+                .multilineTextAlignment(.center)
+                .font(.body)
+                .padding(8)
+            
+            NutritionFoodyView(foody: foody)
+        }
     }
 }
 
